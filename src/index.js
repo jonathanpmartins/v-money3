370aeb1137156ceb8f69596900600add33bb1bf9
Jonathan Martins <web@jonathanmartins.com.br>
2021-05-02 19:52:01 -0300

    naming default export inside entry file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Money3Component from './component.vue'
 import Money3Directive from './directive'
 
+const money = {
+  install: (app) => {
+    app.component('money3', Money3Component)
+    app.directive('money3', Money3Directive)
+  }
+};
+
+export default money
+
 export {
   Money3Component,
   Money3Directive,
@@ -9,11 +18,4 @@ export {
   Money3Directive as VMoney3,
   Money3Component as Money,
   Money3Directive as VMoney,
-}
-
-export default {
-  install: (app) => {
-    app.component('money3', Money3Component)
-    app.directive('money3', Money3Directive)
-  }
 }
