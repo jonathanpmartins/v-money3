73fa87e0561ae133bd85305143520768a1d2d85e
Jonathan Martins <web@jonathanmartins.com.br>
2021-07-30 23:12:07 -0300

    fucking commiting things in the main branch again! This is the puppeteer integration with the first hello world test. Going to dev

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+const tsPreset = require('ts-jest/jest-preset');
+const puppeteerPreset = require('jest-puppeteer/jest-preset');
+
 module.exports = {
-  preset: 'ts-jest',
-  globals: {},
+  ...tsPreset,
+  ...puppeteerPreset,
+  globals: {
+    // test_url: `http://${process.env.HOST || '127.0.0.1'}:${process.env.PORT || 3000}`,
+  },
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue-jest',
---
+++ tests/puppeteer.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment puppeteer
+ */
+
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com');
+  });
+
+  it('should be titled "Google"', async () => {
+    await expect(page.title()).resolves.toMatch('Google');
+  });
+});
