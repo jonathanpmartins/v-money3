c52c0cd38af9ec2141ebcc2fa733682d0dca0905
Jonathan Martins <web@jonathanmartins.com.br>
2021-08-20 19:33:26 -0300

    Babel transpile of ** to pow create errors on tests.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        exclude: ['babel-plugin-transform-exponentiation-operator'],
+      },
+    ],
+  ],
   plugins: [
     ['@babel/transform-runtime'],
   ],
